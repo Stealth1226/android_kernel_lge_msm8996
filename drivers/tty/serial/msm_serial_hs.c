11e3109c8d4c1eca030b0e56ddf1db07dcab7ad4
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-10 21:41:10 -0400

    bluetooth: Remove broadcom drivers and switch bluesleep back to proc
    Change-Id: I93e11204896577a258afcff599b377cbd606e788

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -67,10 +67,6 @@
 #include <linux/platform_data/msm_serial_hs.h>
 #include <linux/msm-bus.h>
 
-#ifdef CONFIG_BT_MSM_SLEEP
-#include <net/bluetooth/bluesleep.h>
-#endif
-
 #include "msm_serial_hs_hwreg.h"
 #define UART_SPS_CONS_PERIPHERAL 0
 #define UART_SPS_PROD_PERIPHERAL 1
@@ -312,16 +308,10 @@ static int msm_hs_ioctl(struct uart_port *uport, unsigned int cmd,
 	switch (cmd) {
 	case MSM_ENABLE_UART_CLOCK: {
 		ret = msm_hs_request_clock_on(&msm_uport->uport);
-#ifdef CONFIG_BT_MSM_SLEEP
-		bluesleep_outgoing_data();
-#endif
 		break;
 	}
 	case MSM_DISABLE_UART_CLOCK: {
 		ret = msm_hs_request_clock_off(&msm_uport->uport);
-#ifdef CONFIG_BT_MSM_SLEEP
-		bluesleep_tx_allow_sleep();
-#endif
 		break;
 	}
 	case MSM_GET_UART_CLOCK_STATUS: {
@@ -1453,11 +1443,6 @@ static void msm_hs_submit_tx_locked(struct uart_port *uport)
 	/* Queue transfer request to SPS */
 	ret = sps_transfer_one(sps_pipe_handle, src_addr, tx_count,
 				msm_uport, flags);
-
-	/* Notify the bluesleep driver of outgoing data, if available. */
-#if defined(CONFIG_BT_MSM_SLEEP) && !defined(CONFIG_LINE_DISCIPLINE_DRIVER)
-	bluesleep_outgoing_data();
-#endif
 
 	MSM_HS_DBG("%s:Enqueue Tx Cmd, ret %d\n", __func__, ret);
 }
@@ -2740,10 +2725,8 @@ static int msm_hs_startup(struct uart_port *uport)
 
 
 	spin_lock_irqsave(&uport->lock, flags);
-#ifdef CONFIG_BT_MSM_SLEEP
 	atomic_set(&msm_uport->client_count, 0);
 	atomic_set(&msm_uport->client_req_state, 0);
-#endif
 	LOG_USR_MSG(msm_uport->ipc_msm_hs_pwr_ctxt,
 			"%s: Client_Count 0\n", __func__);
 	msm_hs_start_rx_locked(uport);
